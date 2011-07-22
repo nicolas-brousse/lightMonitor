4674589ad4211f8151803899b445f039c53305a0
Nicolas BROUSSE <pro@nicolas-brousse.fr>
2011-07-22 23:28:52 +0200

    Update style

--- app/routes.php
+++ app/routes.php
@@ -24,14 +24,14 @@ $app->get('/servers/{ip}',  function () use($controllers) { return $controllers[
 
 
 
-$app->get('/configs/servers',               function () use($controllers) { return $controllers['config']->Index_Action(); })->bind('configs.servers');
-$app->get('/configs/servers/new',           function () use($controllers) { return $controllers['config']->New_Action(); })->bind('configs.servers.new');
-$app->post('/configs/save',         function () use($controllers) { return $controllers['config']->Save_Action(); })->bind('configs.servers.save');
-$app->get('/configs/edit/{ip}',     function () use($controllers) { return $controllers['config']->Edit_Action(); })->bind('configs.servers.edit');
-$app->post('/configs/update/{ip}',  function () use($controllers) { return $controllers['config']->Update_Action(); })->bind('configs.servers.update');
-$app->get('/configs/delete/{ip}',   function () use($controllers) { return $controllers['config']->Delete_Action(); })->bind('configs.servers.delete');
+$app->get('/configs/servers',         function () use($controllers) { return $controllers['config']->Index_Action(); })->bind('configs.servers');
+$app->get('/configs/servers/new',     function () use($controllers) { return $controllers['config']->New_Action(); })->bind('configs.servers.new');
+$app->post('/configs/save',           function () use($controllers) { return $controllers['config']->Save_Action(); })->bind('configs.servers.save');
+$app->get('/configs/edit/{ip}',       function () use($controllers) { return $controllers['config']->Edit_Action(); })->bind('configs.servers.edit');
+$app->post('/configs/update/{ip}',    function () use($controllers) { return $controllers['config']->Update_Action(); })->bind('configs.servers.update');
+$app->get('/configs/delete/{ip}',     function () use($controllers) { return $controllers['config']->Delete_Action(); })->bind('configs.servers.delete');
 
-$app->get('/configs/users',               function () use($controllers) { return $controllers['config']->Index_Action(); })->bind('configs.users');
+$app->get('/configs/users',           function () use($controllers) { return $controllers['config']->Index_Action(); })->bind('configs.users');
 
 
 
@@ -41,34 +41,24 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-$app->get('/favicon.png', function() use ($app) {
+$app->get('/favicon.{ext}', function($ext) use ($app) {
   $state = array_rand(array('on' => true, 'off' => true));
   return new Response(
-          file_get_contents(APPLICATION_BASE_URI . 'public/img/icons/'.$state.'.png'),
+          file_get_contents(APPLICATION_BASE_URI . 'public/img/icons/'.($ext == 'ico' ? 'favicon_' : '').$state.'.'.$ext),
           200,
           array(
-            'Content-Type' => 'image/png',
+            'Content-Type' => ($ext == 'ico' ? 'image/x-icon' : 'image/png'),
             'Cache-Control' => 'no-cache, must-revalidate',
             'Expires' => ' Sat, 26 Jul 1997 05:00:00 GMT',
           )
       );
-});
-$app->get('/favicon.ico', function() use ($app) {
-  $state = array_rand(array('on' => true, 'off' => true));
-  return new Response(
-          file_get_contents(APPLICATION_BASE_URI . 'public/img/icons/favicon_'.$state.'.ico'),
-          200,
-          array(
-            'Content-Type' => 'image/png',
-            'Cache-Control' => 'no-cache, must-revalidate',
-            'Expires' => ' Sat, 26 Jul 1997 05:00:00 GMT',
-          )
-      );
-});
+})
+->assert('ext', '(png|ico)');
+
 
 $app->error(function(\Exception $e) use ($app) {
     if ($e instanceof NotFoundHttpException) {
-        $content = vsprintf('<h1>%d - %s (%s)</h1>', array(
+        $content = vsprintf('<h1>Error %d</h1> <p>%s (%s)</p>', array(
            $e->getStatusCode(),
            Response::$statusTexts[$e->getStatusCode()],
            $app['request']->getRequestUri()
--- public/js/application.js
+++ public/js/application.js
@@ -30,7 +30,7 @@ $(document).ready(function(){
     );
 
 
-    //Minimize Content Box
+    // Minimize Content Box
     $(".content-box-header h3").css({ "cursor":"s-resize" }); // Give the h3 in Content Box Header a different cursor
     $(".closed-box .content-box-content").hide(); // Hide the content of the header if it has the class "closed"
     $(".closed-box .content-box-tabs").hide(); // Hide the tabs in the header if it has the class "closed"
@@ -43,7 +43,7 @@ $(document).ready(function(){
     );
 
 
-    // Content box tabs:
+    // Content box tabs
     $('.content-box .content-box-content div.tab-content').hide(); // Hide the content divs
     $('ul.content-box-tabs li a.default-tab').addClass('current'); // Add the class "current" to the default tab
     $('.content-box-content div.default-tab').show(); // Show the div with class "default-tab"
@@ -52,8 +52,7 @@ $(document).ready(function(){
         $(this).parent().siblings().find("a").removeClass('current'); // Remove "current" class from all tabs
         $(this).addClass('current'); // Add class "current" to clicked tab
         var currentTab = $(this).attr('href'); // Set variable "currentTab" to the value of href of clicked tab
-        $(currentTab).siblings().hide(); // Hide all content divs
-        $(currentTab).show(); // Show the content div with the id equal to the id of clicked tab
+        $(currentTab).siblings().slideUp('normal', function() { $(currentTab).slideDown(); });
         return false;
       }
     );
