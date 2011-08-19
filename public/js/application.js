6127f18536be0593d26241fded4e3d6f1487c605
Nicolas BROUSSE <pro@nicolas-brousse.fr>
2011-08-19 22:59:07 +0200

    Update design

--- public/js/application.js
+++ public/js/application.js
@@ -27,21 +27,21 @@ $(document).ready(function(){
   );
 
   // Accordion
-  $("#main-nav li ul").hide(); // Hide all sub menus
-  $("#main-nav li a.current").parent().find("ul").slideToggle("slow"); // Slide down the current menu item's sub menu
-  $("#main-nav li a.nav-top-item").click( // When a top menu item is clicked...
+  $("#main-nav li ul").hide();
+  $("#main-nav li a.nav-top-item").click(
     function () {
-      $(this).parent().siblings().find("ul").slideUp("normal"); // Slide up all sub menus except the one clicked
-      $(this).next().slideToggle("normal"); // Slide down the clicked sub menu
+      $(this).parent().siblings().find("ul").slideUp("normal");
+      $(this).next().slideToggle("normal");
       return false;
     }
   );
-  $("#main-nav li a.no-submenu").click( // When a menu item with no sub menu is clicked...
+  $("#main-nav li a.no-submenu").click(
     function () {
-      window.location.href=(this.href); // Just open the link instead of a sub menu
+      window.location.href=(this.href);
       return false;
     }
   );
+  $("#main-nav li a.current").click();
 
 
 
