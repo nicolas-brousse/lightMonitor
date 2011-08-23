9c84bf8b7e71fe6ea7ab24783199b4492f770112
Nicolas BROUSSE <pro@nicolas-brousse.fr>
2011-08-23 22:50:43 +0200

    Update db options.php for migration system

--- db/options.php
+++ db/options.php
@@ -1,6 +1,11 @@
 <?php
-return array(
-  'driver'    => 'pdo_sqlite',
-  'path'      => __DIR__ . '/light_monitor.sqlite'
-);
-?>
+
+define('APPLICATION_BASE_URI', dirname(__DIR__).'/');
+
+require_once APPLICATION_BASE_URI.'/vendor/App.php';
+require_once APPLICATION_BASE_URI.'/vendor/yaml/lib/sfYaml.php';
+
+$config = App::loadConfigs(APPLICATION_BASE_URI . '/app/configs/', 'production');
+$config->app['db']['path'] = APPLICATION_BASE_URI . $config->app['db']['path'];
+
+return $config->app['db'];
